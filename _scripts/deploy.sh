206bd180136ebb754cd74f88d1f4e3cf111012f7
blu3id <blu3id@users.noreply.github.com>
2018-04-08 14:51:49 +0100

    Fix staging domain filtering of . in repository name

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,7 +2,12 @@
 # Split on "/", ref: http://stackoverflow.com/a/5257398/689223
 REPO_SLUG_ARRAY=(${TRAVIS_REPO_SLUG//\// })
 REPO_OWNER=${REPO_SLUG_ARRAY[0]}
-REPO_NAME=${REPO_SLUG_ARRAY[1]}
+# replaces "/" or "." with "-"
+# sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
+# Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
+# The length is limited to 253 characters
+# https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
+REPO_NAME=`echo "${REPO_SLUG_ARRAY[1]}" | sed -r 's/[\/|\.]+/\-/g'`
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" != "false" ]
 then
