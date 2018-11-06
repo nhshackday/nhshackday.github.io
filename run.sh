76b99c5140f7cd3f64309d1615fe3c46ec07ed07
Helen Jackson <helenj@ckson.io>
2018-11-06 11:48:36 +0000

    Added --verbose flag to jekyll serve command

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -e
 gem install bundler && \
     bundle install && \
-    bundle exec jekyll serve  --host 0.0.0.0 --force_polling --livereload 
+    bundle exec jekyll serve  --host 0.0.0.0 --force_polling --livereload --verbose
 exec "$@"
