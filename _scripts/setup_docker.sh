dee7fada52021b868e7c46e148675a74ac092589
Helen Jackson <helenj@ckson.io>
2018-11-06 13:52:48 +0000

    Specified version of bundler

--- _scripts/setup_docker.sh
+++ _scripts/setup_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-gem install bundler && \
-    bundle install && \
-    bundle exec jekyll serve  --host 0.0.0.0 --force_polling --livereload --verbose
+gem install bundler -v 1.17.1 && \
+    bundle _1.17.1_ install && \
+    bundle _1.17.1_ exec jekyll serve  --host 0.0.0.0 --force_polling --livereload --verbose
 exec "$@"
