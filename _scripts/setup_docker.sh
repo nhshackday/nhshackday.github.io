089b277792cfe8bf0ca152789785d3c7e4710a86
Mike <blu3id@users.noreply.github.com>
2018-11-14 20:59:13 +0000

    Delete setup_docker.sh

--- _scripts/setup_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-gem install bundler -v 1.17.1 && \
-    bundle _1.17.1_ install && \
-    bundle _1.17.1_ exec jekyll serve  --host 0.0.0.0 --force_polling --livereload --verbose
-exec "$@"
